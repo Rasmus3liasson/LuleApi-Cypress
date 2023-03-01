c2ae014d68fd374455020f56b624ef5bdaae67ff
Rasmus Eliasson <ras.muse@hotmail.com>
2023-03-01 19:41:10 +0100

    2 more test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,3 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
-  });
-});
-
 describe("Test to see if our page i running on digital ocean", () => {
   it("Visits our deployed site on our digital ocean", () => {
     cy.visit("https://lionfish-app-b672u.ondigitalocean.app/");
@@ -15,5 +9,23 @@ describe("Test to see that correct host is applied when server is running", () =
     cy.visit("http://localhost:8080/");
 
     cy.url().should("include", "localhost:8080");
+  });
+});
+
+describe("Find logotype", () => {
+  it("finds 'img' logotype", () => {
+    cy.visit("http://localhost:8080/");
+
+    cy.get(".logotype").should("have.attr", "src", "/static/logo-cinema.png");
+  });
+});
+
+describe("Logotype have a click event", () => {
+  it("clicks logotype", () => {
+    cy.visit("http://localhost:8080/");
+
+    cy.get(".logotype")
+      .should("have.attr", "src", "/static/logo-cinema.png")
+      .click();
   });
 });
